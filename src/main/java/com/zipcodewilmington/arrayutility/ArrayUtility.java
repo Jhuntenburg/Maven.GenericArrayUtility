eeac8f37ba39459fda26eed787c9306d8dce71c9
Joe <Jhuntenburg@gmail.com>
2023-07-26 19:28:02 -0400

    3 working methods

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -8,26 +8,44 @@ import java.util.Arrays;
  */
 public class ArrayUtility<T> {
 
-    private  T[] array;
+    private T[] array;
 
     public ArrayUtility(T[] inputArray) {
         this.array = inputArray;
     }
 
     public Integer countDuplicatesInMerge(T[] arrayToMerge, T valueToEvaluate) {
-        return 0;
+        T[] newArray = (T[]) Array.newInstance(valueToEvaluate.getClass(), array.length + arrayToMerge.length);
+        int count = 0;
+        for (int i = 0; i < array.length; i++) {
+            newArray[i] = array[i];
+
+        }
+        for (int i = array.length; i < array.length + arrayToMerge.length; i++) {
+            newArray[i] = arrayToMerge[i - array.length];
+
+        }
+        for (int i = 0; i < newArray.length; i++) {
+            if (newArray[i] == valueToEvaluate) {
+                count++;
+
+            }
+        }
+
+
+        return count;
     }
-    
-    
+
+
     public T getMostCommonFromMerge(T[] arrayToMerge) {
         return null;
     }
-    
-    
+
+
     public Integer getNumberOfOccurrences(T valueToEvaluate) {
-       int counter = 0;
+        int counter = 0;
         for (int i = 0; i < array.length; i++) {
-            if (array[i] == valueToEvaluate){
+            if (array[i] == valueToEvaluate) {
                 counter++;
             }
 
@@ -35,15 +53,15 @@ public class ArrayUtility<T> {
 
         return counter;
     }
-    
-    
+
+
     public T[] removeValue(T valueToRemove) {
         int size = getNumberOfOccurrences(valueToRemove);
-        T[] newArray = (T[]) Array.newInstance(valueToRemove.getClass(), array.length-size);
+        T[] newArray = (T[]) Array.newInstance(valueToRemove.getClass(), array.length - size);
         int count = 0;
 
         for (int i = 0; i < array.length; i++) {
-            if (array[i] != valueToRemove){
+            if (array[i] != valueToRemove) {
                 newArray[count] = array[i];
                 count++;
             }
@@ -53,9 +71,5 @@ public class ArrayUtility<T> {
         return newArray;
     }
 
-    
 
-   
-
- 
 }
