1bafc268d792c473e3a795448530697e78614f9b
Joe <Jhuntenburg@gmail.com>
2023-07-26 18:58:15 -0400

    2 working methods

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -1,7 +1,61 @@
 package com.zipcodewilmington.arrayutility;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 /**
  * Created by leon on 3/6/18.
  */
-public class ArrayUtility {
+public class ArrayUtility<T> {
+
+    private  T[] array;
+
+    public ArrayUtility(T[] inputArray) {
+        this.array = inputArray;
+    }
+
+    public Integer countDuplicatesInMerge(T[] arrayToMerge, T valueToEvaluate) {
+        return 0;
+    }
+    
+    
+    public T getMostCommonFromMerge(T[] arrayToMerge) {
+        return null;
+    }
+    
+    
+    public Integer getNumberOfOccurrences(T valueToEvaluate) {
+       int counter = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == valueToEvaluate){
+                counter++;
+            }
+
+        }
+
+        return counter;
+    }
+    
+    
+    public T[] removeValue(T valueToRemove) {
+        int size = getNumberOfOccurrences(valueToRemove);
+        T[] newArray = (T[]) Array.newInstance(valueToRemove.getClass(), array.length-size);
+        int count = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] != valueToRemove){
+                newArray[count] = array[i];
+                count++;
+            }
+
+        }
+
+        return newArray;
+    }
+
+    
+
+   
+
+ 
 }
